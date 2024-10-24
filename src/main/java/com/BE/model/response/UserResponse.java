78b8b7c8daaca365d13054ed13eeb1d698f422bb
NguyenHongThinh-2003 <ocbu0212@gmail.com>
2024-10-24 13:10:34 +0700

    Fix/Thinh: Update User Response

--- src/main/java/com/BE/model/response/UserResponse.java
+++ src/main/java/com/BE/model/response/UserResponse.java
@@ -4,12 +4,15 @@ import com.BE.enums.RoleEnum;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
+import java.util.UUID;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserResponse {
+    UUID id;
     String fullName;
     String studentCode;
     String email;
