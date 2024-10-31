19b9824032fb881e4eceeeccaa499a8cfda335d0
DoMinh-2003 <minhlola28@gmail.com>
2024-10-31 15:41:07 +0700

    Feat/Minh--readNotificationById

--- src/main/java/com/BE/controller/NotificationController.java
+++ src/main/java/com/BE/controller/NotificationController.java
@@ -6,8 +6,11 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @SecurityRequirement(name = "api")
@@ -24,6 +27,13 @@ public class NotificationController {
     @PutMapping("/api/notifications")
     public ResponseEntity updateNotification() {
         notificationService.updateNotification();
+        return responseHandler.response(200,"Update notification successfully",
+                null);
+    }
+
+    @PutMapping("/api/notifications/{id}")
+    public ResponseEntity updateNotificationByid(@PathVariable UUID id) {
+        notificationService.updateNotificationByid(id);
         return responseHandler.response(200,"Update notification successfully",
                 null);
     }
