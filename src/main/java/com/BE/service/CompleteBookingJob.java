1858bbd97910522447b87aa0a67e7d830bfb1504
HongThinhThinh <ocbu0212@gmail.com>
2024-11-04 21:37:25 +0700

    Fix/Thinh: Remove Notifications

--- src/main/java/com/BE/service/CompleteBookingJob.java
+++ src/main/java/com/BE/service/CompleteBookingJob.java
@@ -5,6 +5,7 @@ import com.BE.exception.exceptions.NotFoundException;
 import com.BE.model.EmailDetail;
 import com.BE.model.entity.Booking;
 import com.BE.model.entity.TimeFrame;
+import com.BE.model.entity.User;
 import com.BE.repository.BookingRepository;
 import com.BE.repository.TimeFrameRepository;
 import com.BE.service.interfaceServices.INotificationService;
@@ -47,19 +48,25 @@ public class CompleteBookingJob implements Job {
 
         String message = "The booking with ID " + bookingId + " has been completed.";
 
+        // Nạp lại `mentor` từ cơ sở dữ liệu để đảm bảo nó được quản lý
+        User mentor = bookingRepository.findById(bookingId)
+                .orElseThrow(() -> new NotFoundException("Mentor Not Found"))
+                .getMentor();
+
         try {
-            notificationService.createNotification("Booking Completed", message, booking.getMentor(), false);
+//            notificationService.createNotification("Booking Completed", message, mentor, false);
             EmailDetail emailDetail = new EmailDetail();
-            emailDetail.setSubject("Yêu đánh dấu đã hoàn thành cuộc họp");
-            emailDetail.setFullName(booking.getMentor().getFullName());
-            emailDetail.setRecipient(booking.getMentor().getEmail());
+            emailDetail.setSubject("Yêu cầu đánh dấu đã hoàn thành cuộc họp");
+            emailDetail.setFullName(mentor.getFullName());
+            emailDetail.setRecipient(mentor.getEmail());
             emailDetail.setButtonValue("Đánh dấu hoàn thành");
-            emailDetail.setLink("http://localhost:5173/mentor/booking-history?bookingId="+booking.getId());
+            emailDetail.setLink("http://localhost:5173/mentor/booking-history?bookingId=" + booking.getId());
             sendMailUtils.threadSendMailTemplate(emailDetail);
         } catch (Exception e) {
             System.out.println("Error while creating notification: " + e.toString());
         }
         System.out.println("Booking " + bookingId + " has been marked as completed and email sent to mentor.");
     }
+
 
 }
